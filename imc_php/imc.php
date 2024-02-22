2c8d689da266bb1b5e9b3717a9518da48f809471
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-02-22 16:11:44 -0300

    Organization

--- imc_php/imc.php
+++ imc_php/imc.php
@@ -10,6 +10,18 @@
         body{
             text-align: center;
         }
+        main{
+            position: relative;
+            padding: 10px;
+            margin: 10px;
+        }
+        #duzentos{
+            position: relative;
+            width:200px;
+            height:200px;
+            border: dotted 2px black;
+            margin: auto; /* Centraliza no centro */
+        }
     </style>
 </head>
 <body>
@@ -20,15 +32,17 @@
     </header>
     <hr>
     <main>
-        <form action="result.php" method="post">
-            <label for="">Nome: </label>
-            <input type="text" name="nome" id="nome"><br><br>
-            <label for="">Peso (kg): </label>
-            <input type="text" name="peso" id="peso"><br><br>
-            <label for="">Altura (m): </label>
-            <input type="text" name="altura" id="altura"><br><br>
-            <input type="submit" value="IMC">
-        </form>
+        <div id="duzentos">
+            <form action="result.php" method="post">
+                <label for="">Nome: </label>
+                <input type="text" name="nome" id="nome"><br><br>
+                <label for="">Peso (kg): </label>
+                <input type="text" name="peso" id="peso"><br><br>
+                <label for="">Altura (m): </label>
+                <input type="text" name="altura" id="altura"><br><br>
+                <input type="submit" value="IMC">
+            </form>
+        </div>
     </main>
     <hr>
     <footer>Desenvolvido no Instituto Federal IFSP Campus Guarulhos</footer>
